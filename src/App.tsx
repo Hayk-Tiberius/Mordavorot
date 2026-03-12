d2c752a58dbc8d779f02bc247d9848dcb46d1aeb
Hayk <avrecij02@gmail.com>
2026-03-12 07:17:36 +0300

    Intro landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,54 @@
 import "./main.css";
+import intro from "../assets/Intro/_DSC9249.jpg";
 import { CIcon } from "@coreui/icons-react";
 import { cibTelegram, cibInstagram } from "@coreui/icons";
 import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <main>
-      <section className="section1">
-        <div className="menu">
-          <span>
-            <Link to="/">Главная</Link>
-          </span>
-          <span>
-            <Link to="/gallery">Галерея</Link>
-          </span>
-          <span>
-            <Link to="/services">Услуги</Link>
-          </span>
-          <span>
-            <Link to="/contacts">Контакты</Link>
-          </span>
-        </div>
-        <div>
-          <span className="all_photos">All</span>
-        </div>
-        <div className="author_name">Mordavorot</div>
-      </section>
+    <>
+      <img src={intro} alt="Intro" className="intro_photo" />
+      <main>
+        <section className="section1">
+          <div className="menu">
+            <span>
+              <Link to="/">Главная</Link>
+            </span>
+            <span>
+              <Link to="/gallery">Галерея</Link>
+            </span>
+            <span>
+              <Link to="/services">Услуги</Link>
+            </span>
+            <span>
+              <Link to="/contacts">Контакты</Link>
+            </span>
+          </div>
+          <div>
+            <span className="all_photos">All</span>
+          </div>
+          <div className="author_name">Mordavorot</div>
+        </section>
 
-      <section className="section2">
-        <Outlet />
-      </section>
+        <section className="section2">
+          <Outlet />
+        </section>
 
-      <section className="section3">
-        <span className="email_text">
-          <a href="mailto:I@mordavorot.ru">I@mordavorot.ru</a>
-        </span>
-        <div className="icons">
-          <CIcon className="icon_item" icon={cibTelegram} />
-          <CIcon icon={cibInstagram} />
-        </div>
-        <span>
-          *Профиль в Instagram, соцсеть признана <br /> экстремистской организацией и запрещена в РФ
-        </span>
-      </section>
-    </main>
+        <section className="section3">
+          <span className="email_text">
+            <a href="mailto:I@mordavorot.ru">I@mordavorot.ru</a>
+          </span>
+          <div className="icons">
+            <CIcon className="icon_item" icon={cibTelegram} />
+            <CIcon icon={cibInstagram} />
+          </div>
+          <span>
+            *Профиль в Instagram, соцсеть признана <br /> экстремистской организацией и запрещена в
+            РФ
+          </span>
+        </section>
+      </main>
+    </>
   );
 }
 
