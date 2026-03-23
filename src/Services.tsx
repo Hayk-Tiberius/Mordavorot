c8749e7681851c25e7cb4018e56c40afc4155674
Hayk <avrecij02@gmail.com>
2026-03-23 21:05:29 +0300

    Admin panel created page

--- src/Services.tsx
+++ src/Services.tsx
@@ -1,20 +1,70 @@
-import App from "./App";
+import service1 from "../assets/img/services_page/services_photo.jpg";
+import service2 from "../assets/img/services_page/services_video.jpg";
+import service3 from "../assets/img/services_page/services_ai.jpg";
 
 function Services() {
   return (
     <>
       <div className="services">
         <div className="services_items">
-          <img src="" alt="" />
-          Фото
+          <div className="services_items_photo">
+            <img src={service1} alt="" />
+          </div>
+          <div className="services_items_text">
+            <span className="services_items_text_title">Фото</span>
+            <span>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ipsa sunt, sed nihil
+              dicta, sit itaque illo aliquid sapiente quia necessitatibus quasi vitae impedit
+              aperiam dignissimos laborum consequatur dolorum. Non. Nemo error ipsum cumque quam
+              voluptatibus nesciunt mollitia ex, similique non fugit soluta aut quas dolorum
+              reiciendis?
+            </span>
+            <button type="submit" className="btn-17">
+              <span className="text-container">
+                <span className="text">Отправить заявку</span>
+              </span>
+            </button>
+          </div>
         </div>
         <div className="services_items">
-          <img src="" alt="" />
-          Видео
+          <div className="services_items_photo">
+            <img src={service2} alt="" />
+          </div>
+          <div className="services_items_text">
+            <span className="services_items_text_title">Видео</span>
+            <span>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ipsa sunt, sed nihil
+              dicta, sit itaque illo aliquid sapiente quia necessitatibus quasi vitae impedit
+              aperiam dignissimos laborum consequatur dolorum. Non. Nemo error ipsum cumque quam
+              voluptatibus nesciunt mollitia ex, similique non fugit soluta aut quas dolorum
+              reiciendis?
+            </span>
+            <button type="submit" className="btn-17">
+              <span className="text-container">
+                <span className="text">Отправить заявку</span>
+              </span>
+            </button>
+          </div>
         </div>
         <div className="services_items">
-          <img src="" alt="" />
-          ИИ
+          <div className="services_items_photo">
+            <img className="photo_test" src={service3} alt="" />
+          </div>
+          <div className="services_items_text">
+            <span className="services_items_text_title">ИИ</span>
+            <span>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ipsa sunt, sed nihil
+              dicta, sit itaque illo aliquid sapiente quia necessitatibus quasi vitae impedit
+              aperiam dignissimos laborum consequatur dolorum. Non. Nemo error ipsum cumque quam
+              voluptatibus nesciunt mollitia ex, similique non fugit soluta aut quas dolorum
+              reiciendis?
+            </span>
+            <button type="submit" className="btn-17">
+              <span className="text-container">
+                <span className="text">Отправить заявку</span>
+              </span>
+            </button>
+          </div>
         </div>
       </div>
     </>
